726e41cfc54d5744cf9fb931880b417b35210797
kleiver <kleiver615@gmail.com>
2020-04-22 17:23:09 -0400

    instance

---
+++ instance/config.py
@@ -0,0 +1,14 @@
+import os
+import os.path as op
+
+from flask_web_app import file_path
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG = True
+SQLALCHEMY_ECHO = True
+SQLALCHEMY_DATABASE_URI = "postgresql:///flask_web"
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+FLASK_ADMIN_SWATCH = "cerulean"
+SECRET_KEY = "pelispedia"
+UPLOADED_PHOTOS_DEST = file_path
